90fe06392f71186fc8f00f5f2110dbdd2e30c7ac
Claire Pottier <claire_pottier@hotmail.fr>
2022-01-27 19:36:50 +0100

    iteration 3

--- src/pages/AllBeersPage.js
+++ src/pages/AllBeersPage.js
@@ -1,12 +1,46 @@
 import React from 'react';
 import Header from "../components/Header";
+import axios from 'axios';
+
+const apiURL = 'https://ih-beers-api2.herokuapp.com/beers';
 
 class AllBeers extends React.Component {
+    state = {
+        listOfAllBeers: []
+    }
+
+    getAllBeers = () => {
+        axios.get(apiURL)
+            .then((response) => {
+                console.log('data= ', response.data)
+                this.setState({
+                    listOfAllBeers: response.data
+                })
+            })
+            .catch((error) => {
+                console.log('error= ', error)
+            })
+    }
+
+    componentDidMount() {
+        this.getAllBeers()
+    }
+
     render() {
         return(
             <div>
                 <Header />
                 <h2>Beers' List</h2>
+                {this.state.listOfAllBeers.map((eachBeer) => {
+                    return (
+                        <div key={eachBeer._id}>
+                            <img src={eachBeer.image_url}/>
+                            <h1>{eachBeer.name}</h1>
+                            <h2>{eachBeer.tagline}</h2>
+                            <h3>Created by: {eachBeer.contributed_by}</h3>
+                        </div>
+                    )
+                })}
             </div>
         )
     }
